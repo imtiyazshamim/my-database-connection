98cc52b174e6f4e3843dc9f4b3a4e584d26c4f24
SHAMIM IMTIYAZ <70932990+imtiyazshamim@users.noreply.github.com>
2020-12-07 22:02:17 +0530

    Add files via upload
    for my own purpose

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername ="localhost";
+$username ="root";
+$password="";
+
+//create connection
+
+$conn =new mysqli($servername,$username,$password);
+//check connection
+
+if($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+?>
